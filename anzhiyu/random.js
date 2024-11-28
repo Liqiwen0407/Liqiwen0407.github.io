c391a7b5a7221b785390ddbc7bb68230d8aa7790
Liqiwen0407 <2297957816@qq.com>
2024-11-28 10:18:04 +0800

    Site updated: 2024-11-28 10:18:03

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/27/hello-world/"];function toRandomPost(){
+var posts=["2024/11/27/hello-world/","2024/11/28/STC51/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
