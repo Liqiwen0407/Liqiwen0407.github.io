f13064690d7127673e0c914e2666afaa7325224b
Liqiwen0407 <2297957816@qq.com>
2024-11-30 19:18:26 +0800

    Site updated: 2024-11-30 19:18:26

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/27/hello-world/","2024/11/28/STC51/"];function toRandomPost(){
+var posts=["2024/11/27/hello-world/","2024/11/28/STC51/","2024/11/30/hanshuku/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
