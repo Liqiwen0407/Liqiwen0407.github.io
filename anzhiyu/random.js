de7383fd7673757b4d735a1ee3919cc54fd048f4
Liqiwen0407 <2297957816@qq.com>
2024-12-02 16:19:11 +0800

    Site updated: 2024-12-02 16:19:10

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/27/hello-world/","2024/11/28/STC51/","2024/11/30/hanshuku/"];function toRandomPost(){
+var posts=["2024/11/27/hello-world/","2024/11/28/STC51/","2024/11/30/hanshuku/","2024/12/02/CJiChu/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
