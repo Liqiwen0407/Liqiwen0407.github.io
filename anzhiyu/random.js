1b83b39c3167f3a0f29a6178a6888fe55740d353
Liqiwen0407 <2297957816@qq.com>
2025-02-19 02:45:14 +0800

    Site updated: 2025-02-19 02:45:14

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/27/hello-world/","2024/11/28/STC51/","2024/11/30/hanshuku/","2024/12/02/CJiChu/","2025/01/01/network/"];function toRandomPost(){
+var posts=["2024/11/27/hello-world/","2024/11/28/STC51/","2024/11/30/hanshuku/","2024/12/02/CJiChu/","2025/01/01/network/","2025/02/19/3D_Print/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
